a4bef16394a2f5efae0f1ccfa2d155b97142eb36
ctes091x <ctes091x@ctes091x.net>
2026-03-22 16:12:09 +0900

    feat: Multiple config file detection
    `.zshrc` → `.bashrc` → `.bash_profile`の順で探し最初にヒットしたものを変更

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 cd `dirname $0`
-if [ -d ~/.login_bonus_for_shell ]; then
+if [ -d $HOME/.login_bonus_for_shell ]; then
 	echo "再インストールしますか？ (Y / n)"
 	read ans
 	if [ $ans == Y ]; then
-		rm -rf ~/.login_bonus_for_shell
+		rm -rf $HOME/.login_bonus_for_shell
 		echo "旧ファイルを削除しました"
-		mkdir ~/.login_bonus_for_shell
-		cp ./* ~/.login_bonus_for_shell
+		mkdir $HOME/.login_bonus_for_shell
+		cp ./* $HOME/.login_bonus_for_shell
 		echo "ファイルをコピーしました"
 	else
 		echo "インストールを中断しました"
@@ -15,39 +15,53 @@ if [ -d ~/.login_bonus_for_shell ]; then
 	fi
 else
 	echo "インストールを開始します"
-	mkdir ~/.login_bonus_for_shell
-	cp ./* ~/.login_bonus_for_shell
+	mkdir $HOME/.login_bonus_for_shell
+	cp ./* $HOME/.login_bonus_for_shell
 	echo "ファイルをコピーしました"
 fi
-if [ -f ~/.bash_profile ]; then
+
+if [ -f $HOME/.zshrc ]; then
+	echo ".zshrcが見つかりました"
+  target="$HOME/.zshrc"
+elif [ -f $HOME/.bashrc ]; then
+	echo ".bashrcが見つかりました"
+  target="$HOME/.bashrc"
+elif [ -f $HOME/.bash_profile ]; then
 	echo ".bash_profileが見つかりました"
+  target="$HOME/.bash_profile"
 else
-	touch ~/.bash_profile
+	touch $HOME/.bash_profile
 	echo ".bash_profileを作成しました"
+  target="$HOME/.bash_profile"
 fi
-check=`grep -ci "cd ~/.login_bonus_for_shell" ~/.bash_profile`
+
+check=`grep -ci "cd $HOME/.login_bonus_for_shell" $target`
+
 if [ $check -eq 0 ]; then
 	ver=`python -V 2>&1 | cut -c 8`
 	if [ $? -eq 127 ]; then
 		echo "Python3またはPython2が必要です"
-		rm -rf ~/.login_bonus_for_shell
+		rm -rf $HOME/.login_bonus_for_shell
 		echo "インストールを中断しました"
 		exit 1
 	fi
+
 	dec=`expr "$ver" = "3"`
 	if [ $dec -eq 1 ]; then
 		echo "Python3を確認しました"
-		echo "cd ~/.login_bonus_for_shell" >> ~/.bash_profile
-		echo "python logger.py" >> ~/.bash_profile
+		echo "cd $HOME/.login_bonus_for_shell" >> $target
+		echo "python logger.py" >> $target
 	fi
+
 	dec=`expr "$ver" = "2"`
 	if [ $dec -eq 1 ]; then
 		echo "Python2を確認しました"
-		echo "cd ~/.login_bonus_for_shell" >> ~/.bash_profile
-		echo "python logger_for_python2.py" >> ~/.bash_profile
+		echo "cd $HOME/.login_bonus_for_shell" >> $target
+		echo "python logger_for_python2.py" >> $target
 	fi
-	echo "cd ~/" >> ~/.bash_profile
-	echo ".bash_profileを変更しました"
+
+	echo "cd $HOME/" >> $target
+	echo "$target を変更しました"
 	echo "インストール完了"
 	exit 0
 else
